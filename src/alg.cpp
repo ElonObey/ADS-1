59256e3aa3cb3b26e4b23f2447dc6c07dd06c269
ElonObey <gulin1999@mail.ru>
2021-04-01 19:12:13 +0300

    Gulin Dmitriy
    Complite task ADS-1

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,28 @@
-// Copyright 2021 NNTU-CS
-
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int left_border = 0;
+int right_border = SIZE_ARR;
+int mid = 0;
+int desir_position = 0;
+int buff = 0;
+while (left_border<right_border){
+mid = (left_border + right_border) / 2;
+if (arr[mid] > input) right_border = mid - 1;
+else if (arr[mid] < input) left_border = mid + 1;
+else if (arr[mid] == input) {
+desir_position = mid;
+break;}
+else return 0;}
+if (desir_position >= 0) {
+for (int j = desir_position; arr[j] == input; j++){
+if (arr[j] == input)
+buff++;
+}
+for (int i = desir_position - 1; arr[i] == input; i--)
+{
+if (arr[i] == input)
+buff++;
+}
+}
+else return 0;
+return buff;
 }
