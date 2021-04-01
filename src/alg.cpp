e3652c7c7c6f200d23d83a195db1cd0a45da50a5
ElonObey <gulin1999@mail.ru>
2021-04-01 19:25:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,16 +4,17 @@ int right_border = SIZE_ARR;
 int mid = 0;
 int desir_position = 0;
 int buff = 0;
+  
 while (left_border < right_border) {
-mid = (left_border + right_border) / 2;
-if (arr[mid] > input) right_border = mid - 1;
-else if (arr[mid] < input) left_border = mid + 1;
-else if (arr[mid] == input)
-{ desir_position = mid;
-break;}
+ mid = (left_border + right_border) / 2;
+  if (arr[mid] > input) right_border = mid - 1;
+   else if (arr[mid] < input) left_border = mid + 1;
+   else if (arr[mid] == input)
+    { desir_position = mid;
+     break;}
 else
   return 0;
-} 
+}
   if (desir_position >= 0) {
 for (int j = desir_position; arr[j] == input; j++) {
 if (arr[j] == input)
@@ -22,7 +23,7 @@ for (int i = desir_position - 1; arr[i] == input; i--) {
 if (arr[i] == input)
 buff++; }
 }
-else
+ else
   return 0;
 return buff;
 }
